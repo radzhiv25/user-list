594b3de9e24965838ffe97c7b635b6974c29adef
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-10-04 13:43:19 +0530

    feat. Basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-max mt-auto mx-auto p-5'>
+        <p className='text-gray-500 font-semibold'>Made with React and Tailwind</p>
+    </div>
+  )
+}
+
+export default Footer
